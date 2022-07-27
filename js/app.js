b45f59eb13f9824a0c8d2b9cc981440f5257970a
Tyler Main <tyler.l.main@gmail.com>
2022-07-27 18:09:15 -0500

    added products to local storage

--- js/app.js
+++ js/app.js
@@ -14,7 +14,19 @@ let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 
 let resultsBtn = document.getElementById('show-results-btn');
-let resultsList = document.getElementById('results-list');
+// let resultsList = document.getElementById('results-list');
+
+// *** LOCAL STORAGE CONTINUES ***
+
+// STEP 3: GET DATA OUT OF LOCAL STORAGE
+
+let retreivedProducts = localStorage.getItem('myProducts');
+
+console.log('retreivedProducts', retreivedProducts);
+
+let parsedProducts = JSON.parse(retreivedProducts);
+
+console.log('parsed Products >>>', parsedProducts);
 
 // *** CONSTRUCTOR FUNCTION ***
 
@@ -28,47 +40,67 @@ function Product(name, photoExtension = 'jpg'){
 }
 
 //  *** OBJECT CREATION ***
+if (retreivedProducts){
+  allProducts = parsedProducts;
+} else {
 
-new Product('bag');
-new Product('banana');
-new Product('boots');
-new Product('breakfast');
-new Product('bubblegum');
-new Product('chair');
-new Product('cthulhu');
-new Product('dog-duck');
-new Product('dragon');
-new Product('pen');
-new Product('pet-sweep');
-new Product('scissors');
-new Product('shark');
-new Product('sweep', 'png');
-new Product('tauntaun');
-new Product('unicorn');
-new Product('water-can');
-new Product('wine-glass');
+  // REBUILD PRODUCTS INSTANCES - run parsed data back through the Constructor
+  // for (let i = 0; i < parsedProducts.length; i++) {
+  //   if (parsedProducts[i].name === 'sweep') {
+  //     let reconstructedSweep = new Product(parsedProducts[i].name, 'png');
+  //     reconstructedSweep.views = parsedProducts[i].views;
+  //     reconstructedSweep.votes = parsedProducts[i].votes;
+  //   } else {
+  //     let reconstructedProduct = new Product(parsedProduct[i].name);
+  //     reconstructedProduct.views = parsedProducts[i].views;
+  //     reconstructedProduct.votes = parsedProducts[i].votes;
+  //   }
+  // }
 
+  new Product('bag');
+  new Product('banana');
+  new Product('boots');
+  new Product('breakfast');
+  new Product('bubblegum');
+  new Product('chair');
+  new Product('cthulhu');
+  new Product('dog-duck');
+  new Product('dragon');
+  new Product('pen');
+  new Product('pet-sweep');
+  new Product('scissors');
+  new Product('shark');
+  new Product('sweep', 'png');
+  new Product('tauntaun');
+  new Product('unicorn');
+  new Product('water-can');
+  new Product('wine-glass');
+}
 
+console.log('allProducts from Constuctor >>>', allProducts);
 // *** HELPER FUNCTIONS ***
 
 function randomIndexGenerator(){
   return Math.floor(Math.random() * allProducts.length);
 }
 
-function renderImgs(){
-  let imgOneIndex = randomIndexGenerator();
-  let imgTwoIndex = randomIndexGenerator();
-  let imgThreeIndex = randomIndexGenerator();
+let productIndexArray = [];
 
-  while (imgOneIndex === imgTwoIndex){
-    imgTwoIndex = randomIndexGenerator();
+function renderImgs() {
+  while (productIndexArray.length < 6){
+    let randomNum = randomIndexGenerator();
+    if (!productIndexArray.includes(randomNum)) {
+      productIndexArray.push(randomNum);
+    }
   }
-  while (imgOneIndex === imgThreeIndex){
-    imgThreeIndex = randomIndexGenerator();
-  }
-  while (imgTwoIndex === imgThreeIndex){
-    imgThreeIndex = randomIndexGenerator();
-  }
+
+  let imgOneIndex = productIndexArray.shift();
+  let imgTwoIndex = productIndexArray.shift();
+  let imgThreeIndex = productIndexArray.shift();
+  // console.log([imgOneIndex,imgTwoIndex,imgThreeIndex]);
+  // console.log(allProducts);
+
+  console.log(allProducts[imgOneIndex]);
 
   imgOne.src = allProducts[imgOneIndex].photo;
   imgOne.alt = allProducts[imgOneIndex].name;
@@ -103,21 +135,109 @@ function handleClick(event){
   renderImgs();
 
   if(totalVotes === 0){
+    //  *** LOCAL STORAGE STARTS HERE ***
+
+    // STEP 1: STRINGIFY THE DATA
+
+    let stringifiedProducts = JSON.stringify(allProducts);
+
+    console.log('stringified products >>>', stringifiedProducts);
+
+    // STEP 2: ADD TO LOCAL STORAGE
+
+    localStorage.setItem('myProducts', stringifiedProducts);
+
     imgContainer.removeEventListener('click', handleClick);
+
   }
 }
 
 function handleShowResults(){
   if(totalVotes === 0){
-    for(let i = 0; i < allProducts.length; i++){
-      let liElem = document.createElement('li');
-      liElem.textContent = `${allProducts[i].name}: views: ${allProducts[i].views}, votes: ${allProducts[i].votes}`;
-      resultsList.appendChild(liElem);
-    }
+    renderChart();
+    // for(let i = 0; i < allProducts.length; i++){
+    //   let liElem = document.createElement('li');
+    //   liElem.textContent = `${allProducts[i].name}: views: ${allProducts[i].views}, votes: ${allProducts[i].votes}`;
+    //   resultsList.appendChild(liElem);
+    // }
     resultsBtn.removeEventListener('click', handleShowResults);
   }
 }
-// *** EVENT LISTENERS ***
 
+
+// CHART DEMO
+
+// *** CANVAS ELEMENT NEEDED TO RENDER THE CHART ***
+let canvasElem = document.getElementById('my-chart');
+
+function renderChart() {
+
+  // *** CREATING EMPTY ARRAYS TO POPULATE WITH THE INFO FOR OUR CHART ***
+  let productNames = [];
+  let productVotes = [];
+  let productViews = [];
+
+  // *** THIS FOR LOOP TAKES ALL THE DATA AFTER THE VOTING ROUNDS ARE COMPLETED AND POPULATES THE ARRAYS CREATED ABOVE ***
+
+  for(let i = 0; i < allProducts.length; i++){
+    productNames.push(allProducts[i].name);
+    productVotes.push(allProducts[i].votes);
+    productViews.push(allProducts[i].views);
+  }
+
+  // *** CONFIG OBJECT THAT CHART.JS USES TO RENDER THE CHART ***
+  let myObj = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Votes',
+        data: productVotes,
+        backgroundColor: [
+          'midnight blue'
+        ],
+        borderColor: [
+          'midnight blue'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: productViews,
+        backgroundColor: [
+          'grey'
+        ],
+        borderColor: [
+          'midnight blue'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        },
+      },
+      plugins: {
+        legend: {
+          labels:{
+            color: 'white',
+            padding: 30
+          },
+        }
+      }
+    }
+  };
+
+  new Chart(canvasElem, myObj);
+}
+
+
+// const ctx = document.getElementById('myChart').getContext('2d');
+
+
+
+// *** EVENT LISTENERS ***
 imgContainer.addEventListener('click', handleClick);
 resultsBtn.addEventListener('click', handleShowResults);
